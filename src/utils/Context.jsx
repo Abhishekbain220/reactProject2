6db90eb28260a00b00d53847439e93a983ee8d24
abhishekbain220 <you@example.co>
2024-11-13 16:29:44 +0530

    .

--- src/utils/Context.jsx
+++ src/utils/Context.jsx
@@ -3,7 +3,7 @@ import { createContext, useEffect, useState } from "react";
 
 export let Context=createContext()
 export let ContextProvider= (props)=>{
-    const [products, setProducts] = useState(JSON.parse(localStorage.getItem("products"))||null)
+    const [products, setProducts] = useState(JSON.parse(localStorage.getItem("products"))||[])
     // let getProducts=async ()=>{
     //     let {data}=await axios("/products")
     //     setProducts(data)
